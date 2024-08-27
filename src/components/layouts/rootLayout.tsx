1ae3ead677b00871fd8625df365da063b199d2f6
bethmij <bethmij@gmail.com>
2024-08-27 18:21:22 +0530

    customize root layout

--- src/components/layouts/rootLayout.tsx
+++ src/components/layouts/rootLayout.tsx
@@ -1,5 +1,11 @@
+import {Outlet} from "react-router-dom";
+
 export const RootLayout = () => {
     return (
-        <></>
+        <>
+            <div className="flex-1 h-screen w-screen bg-[url('@/assets/img.png')] bg-center">
+                <Outlet/>
+            </div>
+        </>
     );
 };
